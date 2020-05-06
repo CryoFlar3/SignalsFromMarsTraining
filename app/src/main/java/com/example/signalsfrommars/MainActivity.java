90f19f160b2b7c550bd2cd7a221d96a4beb02a51
CryoFlar3 <nyjel.dukes@gmail.com>
2020-05-06 15:53:12 -0400

    Add String resources

--- app/src/main/java/com/example/signalsfrommars/MainActivity.java
+++ app/src/main/java/com/example/signalsfrommars/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.signalsfrommars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,13 +27,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
-                startStory();
+                startStory(name);
             }
         });
     }
 
-    private void startStory() {
-
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/signalsfrommars/StoryActivity.java
+++ app/src/main/java/com/example/signalsfrommars/StoryActivity.java
@@ -2,13 +2,24 @@ package com.example.signalsfrommars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StoryActivity extends AppCompatActivity {
+
+    private static final String TAG = StoryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()){
+            name = "Friend";
+        }
+        Log.d(TAG, name);
     }
 }
